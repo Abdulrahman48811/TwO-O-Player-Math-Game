b6920abd29a45fb2d839387b255643153ab96c18
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-02-03 13:44:13 -0800

    completed

--- game.rb
+++ game.rb
@@ -5,26 +5,35 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @current = player1
   end 
   def startGame()
     puts "game started with #{@player1.name} #{@player2.name}"
-    while player1.lives !== 0 || player2.lives !== 0
-      
-    askQuestion()
-    answer = gets.chomp
-    evaluateQuestion(answer)
+    while @player1.lives != 0 || @player2.lives != 0
+      askQuestion()
+      answer = gets.chomp
+      evaluateQuestion(answer)
+      switchPlayer
     end
-end 
-def askQuestion()
-@question = Question.new(@player1)
-puts "What does #{@question.number1} plus #{@question.number2} equal"
-end
-def evaluateQuestion(answer)
-  # puts "#{answer}"
-  if answer.to_i == @question.answer.to_i
-    puts "Correct!" 
-  else 
-    puts "Seriously? No!"
+  end 
+  def askQuestion()
+    @question = Question.new(@player1)
+    puts "#{@current.name} What does #{@question.number1} plus #{@question.number2} equal"
   end
-end
+  def evaluateQuestion(answer)
+    # puts "#{answer}"
+    if answer.to_i == @question.answer.to_i
+      puts "Correct!" 
+    else 
+      puts "Seriously? No!"
+      @current.reduce_lives
+    end
+  end
+  def switchPlayer()
+    if @current == @player1 
+      @current = @player2
+    else 
+      @current = @player1
+    end
+  end
 end 
